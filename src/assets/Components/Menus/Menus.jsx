d0ccc65b42422d879c07802c3d777a9c56bb985f
Kavinda789 <127042694+Kavinda789@users.noreply.github.com>
2024-12-02 23:33:23 +0530

    12/2-first

--- src/assets/Components/Menus/Menus.jsx
+++ src/assets/Components/Menus/Menus.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
+import { MenuData } from "../../assets";
+import { motion } from "framer-motion";
+import { FedeLeft } from "../Utility/animation";
 
 function Menus() {
   return (
-    <div>
-      asasasas
-    </div>
-  )
+    <section>
+      <div className="container pt-12 pb-20">
+        <motion.h1
+          initial={{ opacity: 0, x: -200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 0.3 }}
+          className="text-2xl font-bold text-left pb-10 uppercase"
+        >
+          Our Menu
+        </motion.h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+          {MenuData.map((menu) => (
+            <motion.div
+              variants={FedeLeft(menu.delay)}
+              initial="hidden"
+              whileInView={"visible"}
+              whileHover={{ scale: 1.1 }}
+              className="bg-white rounded-3xl px-4 py-2 shadow-[0_0_22px_0_rgba(0,0,0,0.15)]
+            flex flex-row justify-around items-center gap-6"
+            >
+              <img
+                src={menu.ImgLink}
+                alt="Image"
+                className="w-[60px]
+                mb-4 scale-110 transform -translate-y-6"
+              />
+              <div>
+                <h1 className="text-lg font-semibold">{menu.name}</h1>
+                <p className="text-lg font-semibold text-secondary">
+                  {menu.price}
+                </p>
+              </div>
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Menus
+export default Menus;
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -26,10 +26,37 @@ export const Menu = [
   },
 ];
 
+import apple from "../assets/Images/apple.png";
+import orange from "../assets/Images/orange.png";
+import avocado from "../assets/Images/avocado.png";
+import cherry from "../assets/Images/cherry.png";
 export const MenuData = [
   {
     id: 1,
     name: "Fresh Red Apples",
+    ImgLink: apple,
     price: "$3.99",
+    delay: 0.3,
+  },
+  {
+    id: 2,
+    name: "Fresh Oranges",
+    ImgLink: orange,
+    price: "$4.99",
+    delay: 0.6,
+  },
+  {
+    id: 3,
+    name: "Fresh Avocado",
+    ImgLink: avocado,
+    price: "$5.99",
+    delay: 0.9,
+  },
+  {
+    id: 4,
+    name: "Fresh Cherries",
+    ImgLink: cherry,
+    price: "$2.99",
+    delay: 1.2,
   },
 ];
